e672db2ce7b9e73d1612db8ca2054f00bec51a48
georgesk <georgesk@debian.org>
2017-04-08 19:13:40 +0200

    first non-bugged release

--- blocks/__init__.py
+++ blocks/__init__.py
@@ -3,13 +3,12 @@ from __future__ import print_function
 import os,string,time
 from collections import OrderedDict
 import time, os
-from CommunicationHandlerQt import communicationHandler
 import numpy as np
 from PyQt4 import QtGui,QtCore
 import pyqtgraph as pg
 import pyqtgraph.exporters
 
-#from templates import ui_layout as layout
+from templates import ui_compozer as compozer
 from utilities.fileBrowser import fileBrowser
 from utilities.expeyesWidgets import expeyesWidgets
 from expeyes import eyemath17 as eyemath
@@ -20,7 +19,11 @@ gettext.bindtextdomain("expeyes")
 gettext.textdomain('expeyes')
 _ = gettext.gettext
 
-#class AppWindow(QtGui.QMainWindow, layout.Ui_MainWindow,expeyesWidgets):
+class AppWindow(QtGui.QMainWindow, compozer.Ui_MainWindow):
+
+    def __init__(self, parent=None):
+        super(self.__class__, self).__init__(parent)
+        self.setupUi(self)
 
 if __name__ == "__main__":
 	app = QtGui.QApplication(sys.argv)
