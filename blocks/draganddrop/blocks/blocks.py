a856879b21a48796347336a791f5b93797685487
Georges Khaznadar <georgesk@debian.org>
2017-04-16 12:20:10 +0200

    fixed bugs with drag/drop weird translations

--- blocks/draganddrop/blocks/blocks.py
+++ blocks/draganddrop/blocks/blocks.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8; mode: python; indent-tabs-mode: t; tab-width:4 -*-
 ############################################################################
 #
 #  Copyright (C) 2017 Georges Khaznadar <georgesk@debian.org>
@@ -22,253 +22,262 @@ from component import Component, acceptedFormats
 
 class BlockWidget(QtGui.QWidget):
 
-    def __init__(self, parent=None):
-        super(BlockWidget, self).__init__(parent)
+	def __init__(self, parent=None):
+		super(BlockWidget, self).__init__(parent)
 
-        self.components = []
-        self.hot = None # when it is a QPoint, something is hot there
+		self.comp = None #dragged component
+		self.components = []
+		self.hot = None # when it is a QPoint, something is hot there
 
-        self.setAcceptDrops(True)
-        self.setMinimumSize(400, 400)
-        self.hotPx={
-            "red": QtGui.QPixmap(":/hot/hot-red.svg"),
-        }
+		self.setAcceptDrops(True)
+		self.setMinimumSize(400, 400)
+		self.hotPx={
+			"red": QtGui.QPixmap(":/hot/hot-red.svg"),
+		}
 
-    def clear(self):
-        self.components = []
-        self.hot=QtCore.QPoint(200,200)
-        self.update()
+	def clear(self):
+		self.components = []
+		self.hot=None
+		self.update()
 
-    def dragEnterEvent(self, event):
-        if acceptedFormats(event):
-           event.accept()
-        else:
-            event.ignore()
+	def dragEnterEvent(self, event):
+		if acceptedFormats(event):
+		   event.accept()
+		else:
+			event.ignore()
 
-    def dragMoveEvent(self, event):
-        if acceptedFormats(event):
-            print(dir(event))
-            event.setDropAction(QtCore.Qt.MoveAction)
-            event.accept()
-        else:
-            event.ignore()
-        return
+	def dragMoveEvent(self, event):
+		f=acceptedFormats(event)
+		if f:
+			data = event.mimeData().data(f[0])
+			comp=Component.unserialize(data)
+			offset=comp.hotspot
+			print("GRRR===============", comp, event.pos(), -offset)
+			for sp in comp.snapPoints:
+				hovering=event.pos()-offset+sp
+				print("GRRR", sp.text,"hover", hovering)
+				
+			event.setDropAction(QtCore.Qt.MoveAction)
+			event.accept()
+		else:
+			event.ignore()
+		return
 
-    def dropEvent(self, event):
-        comp=Component.unserializeFromEvent(event)
-        if comp:
-            self.components.append(comp)
-            self.update(comp.rect)
-            event.setDropAction(QtCore.Qt.MoveAction)
-            event.accept()
-        else:
-            event.ignore()
+	def dropEvent(self, event):
+		comp=Component.unserializeFromEvent(event)
+		if comp:
+			self.components.append(comp)
+			self.update(comp.rect)
+			event.setDropAction(QtCore.Qt.MoveAction)
+			event.accept()
+		else:
+			event.ignore()
 
 
-    def mousePressEvent(self, event):
-        comps=self.targetComps(event.pos())
-        if not comps:
-            return
-        comp = comps[-1]
-        index=self.components.index(comp)
-        comp=copy.copy(comp)
+	def mousePressEvent(self, event):
+		comps=self.targetComps(event.pos())
+		if not comps:
+			return
+		comp = comps[-1]
+		index=self.components.index(comp)
+		comp=copy.copy(comp)
 
-        del self.components[index]
+		del self.components[index]
 
-        self.update(comp.rect)
+		self.update(comp.rect)
 
-        comp.hotspot=QtCore.QPoint(event.pos() - comp.rect.topLeft())
-        itemData = comp.serialize()
-        drag=comp.makeDrag(self)
+		comp.hotspot=QtCore.QPoint(event.pos() - comp.rect.topLeft())
+		itemData = comp.serialize()
+		drag=comp.makeDrag(self)
 
-        if drag.exec_(QtCore.Qt.MoveAction) != QtCore.Qt.MoveAction:
-            self.components.insert(index, comp)
-            self.update()
+		if drag.exec_(QtCore.Qt.MoveAction) != QtCore.Qt.MoveAction:
+			self.components.insert(index, self.comp)
+			self.update()
 
-    def paintEvent(self, event):
-        painter = QtGui.QPainter()
-        painter.begin(self)
-        painter.fillRect(event.rect(), QtCore.Qt.white)
-        
-        l=[(c.rect, c.pixmap) for c in self.components]
-        for rect, pixmap in l:
-            painter.drawPixmap(rect, pixmap)
+	def paintEvent(self, event):
+		painter = QtGui.QPainter()
+		painter.begin(self)
+		painter.fillRect(event.rect(), QtCore.Qt.white)
+		
+		l=[(c.rect, c.pixmap) for c in self.components]
+		for rect, pixmap in l:
+			painter.drawPixmap(rect, pixmap)
 
-        # hot indicator
-        if self.hot:
-            # paint a circle around
-            px=self.hotPx["red"]
-            w,h = px.size().width(), px.size().height()
-            painter.drawPixmap(
-                QtCore.QRect(
-                    QtCore.QPoint(self.hot.x()-w/2, self.hot.y()-h/2),
-                    px.size()),
-                px)
-        painter.end()
+		# hot indicator
+		if self.hot:
+			# paint a circle around
+			px=self.hotPx["red"]
+			w,h = px.size().width(), px.size().height()
+			painter.drawPixmap(
+				QtCore.QRect(
+					QtCore.QPoint(self.hot.x()-w/2, self.hot.y()-h/2),
+					px.size()),
+				px)
+		painter.end()
 
-    def targetComps(self, position):
-        """
-        returns the list of components
-        under a mouse click; the topmost component comes last.
-        """
-        comps = [c for c in self.components if c.rect.contains(position)]
-        return comps
+	def targetComps(self, position):
+		"""
+		returns the list of components
+		under a mouse click; the topmost component comes last.
+		"""
+		comps = [c for c in self.components if c.rect.contains(position)]
+		return comps
 
 class componentsList(QtGui.QListWidget):
-    def __init__(self, parent=None):
-        super(componentsList, self).__init__(parent)
+	def __init__(self, parent=None):
+		super(componentsList, self).__init__(parent)
 
-        self.setDragEnabled(True)
-        self.setViewMode(QtGui.QListView.IconMode)
-        self.setIconSize(QtCore.QSize(60, 60))
-        self.setSpacing(10)
-        self.setAcceptDrops(True)
-        self.setDropIndicatorShown(True)
-        self.setSizePolicy(QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed,
-                QtGui.QSizePolicy.Expanding))
-        self.setMinimumSize(200, 400)
-        self.setMaximumSize(200, 4000)
+		self.setDragEnabled(True)
+		self.setViewMode(QtGui.QListView.IconMode)
+		self.setIconSize(QtCore.QSize(60, 60))
+		self.setSpacing(10)
+		self.setAcceptDrops(True)
+		self.setDropIndicatorShown(True)
+		self.setSizePolicy(QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed,
+				QtGui.QSizePolicy.Expanding))
+		self.setMinimumSize(200, 400)
+		self.setMaximumSize(200, 4000)
 
-    def dragEnterEvent(self, event):
-        if acceptedFormats(event):
-            event.accept()
-        else:
-            event.ignore()
+	def dragEnterEvent(self, event):
+		if acceptedFormats(event):
+			event.accept()
+		else:
+			event.ignore()
 
-    def dragMoveEvent(self, event):
-        if acceptedFormats(event):
-            event.setDropAction(QtCore.Qt.MoveAction)
-            event.accept()
-        else:
-            event.ignore()
+	def dragMoveEvent(self, event):
+		if acceptedFormats(event):
+			event.setDropAction(QtCore.Qt.MoveAction)
+			event.accept()
+		else:
+			event.ignore()
 
-    def dropEvent(self, event):
-        comp=Component.unserializeFromEvent(event)
-        if comp:
-            # components of type 1 can be duplicated
-            # so they should not be appended to the list
-            if comp.mimetype.contains("image/x-Block-1"):
-                pass
-            elif comp.mimetype.contains("image/x-Block-2"):
-                self.addPiece(comp)
+	def dropEvent(self, event):
+		comp=Component.unserializeFromEvent(event)
+		if comp:
+			# components of type 1 can be duplicated
+			# so they should not be appended to the list
+			if comp.mimetype.contains("image/x-Block-1"):
+				pass
+			elif comp.mimetype.contains("image/x-Block-2"):
+				self.addPiece(comp)
 
-            event.setDropAction(QtCore.Qt.MoveAction)
-            event.accept()
-        else:
-            event.ignore()
+			event.setDropAction(QtCore.Qt.MoveAction)
+			event.accept()
+		else:
+			event.ignore()
 
-    def newComponent(self, comp):
-        item=self.addPiece(comp)
-        self.insertItem(0, item)
-        return
+	def newComponent(self, comp):
+		item=self.addPiece(comp)
+		self.insertItem(0, item)
+		return
 
-    def addPiece(self, comp):
-        """
-        adds a Component instance,
-        and returns the QListWidgetItem created
-        """
-        ident=QtCore.QString(comp.ident)
-        for i in range(self.count()):
-            if self.item(i).ident == ident:
-                self.item(i).setHidden(False)
-                return
-        blockItem = QtGui.QListWidgetItem(self)
-        comp.toListWidgetItem(blockItem)
-        return blockItem
+	def addPiece(self, comp):
+		"""
+		adds a Component instance,
+		and returns the QListWidgetItem created
+		"""
+		ident=QtCore.QString(comp.ident)
+		for i in range(self.count()):
+			if self.item(i).component.ident == ident:
+				self.item(i).setHidden(False)
+				return
+		blockItem = QtGui.QListWidgetItem(self)
+		comp.toListWidgetItem(blockItem)
+		return blockItem
 
-    def currentComponent(self):
-        item = self.currentItem()
-        return Component.fromListWidgetItem(item)
-        
-        
-    def startDrag(self, supportedActions):
-        component=self.currentComponent()
-        drag=component.makeDrag(self)
+	def currentComponent(self):
+		item = self.currentItem()
+		return Component.fromListWidgetItem(item)
+		
+		
+	def startDrag(self, supportedActions):
+		component=self.currentComponent()
+		drag=component.makeDrag(self)
 
-        if drag.exec_(QtCore.Qt.MoveAction) == QtCore.Qt.MoveAction:
-            # components of type 1 can be duplicated
-            # so they should not be hidden from the list
-            if component.mimetype.contains("image/x-Block-1"):
-                pass
-            elif component.mimetype.contains("image/x-Block-2"):
-                self.currentItem().setHidden(True)
+		if drag.exec_(QtCore.Qt.MoveAction) == QtCore.Qt.MoveAction:
+			# components of type 1 can be duplicated
+			# so they should not be hidden from the list
+			if component.mimetype.contains("image/x-Block-1"):
+				pass
+			elif component.mimetype.contains("image/x-Block-2"):
+				self.currentItem().setHidden(True)
 
 
 class MainWindow(QtGui.QMainWindow):
-    def __init__(self, parent=None):
-        super(MainWindow, self).__init__(parent)
+	def __init__(self, parent=None):
+		super(MainWindow, self).__init__(parent)
 
-        self.BlockImage = QtGui.QPixmap()
+		self.BlockImage = QtGui.QPixmap()
 
-        self.setupMenus()
-        self.setupWidgets()
+		self.setupMenus()
+		self.setupWidgets()
 
-        self.setWindowTitle("Block")
-        
-    def load(self):
-        """
-        Loads a component composition
-        """
-        return
-        
-    def save(self):
-        """
-        Saves the current component composition
-        """
-        return
-        
-    def saveAs(self, filename=None):
-        """
-        Saves the current component composition in a new file
-        @param filename name of the file, defaults to None
-        """
-        return
-        
-    def loadComponents(self, path=None):
-        self.componentsList.clear()
-        self.BlockWidget.clear()
-        cList=Component.listFromRC()
-        for c in cList:
-            self.componentsList.newComponent(c)
-        return
+		self.setWindowTitle("Block")
+		
+	def load(self):
+		"""
+		Loads a component composition
+		"""
+		return
+		
+	def save(self):
+		"""
+		Saves the current component composition
+		"""
+		return
+		
+	def saveAs(self, filename=None):
+		"""
+		Saves the current component composition in a new file
+		@param filename name of the file, defaults to None
+		"""
+		return
+		
+	def loadComponents(self, path=None):
+		self.componentsList.clear()
+		self.BlockWidget.clear()
+		cList=Component.listFromRC()
+		for c in cList:
+			self.componentsList.newComponent(c)
+		return
 
-    def setupMenus(self):
-        fileMenu = self.menuBar().addMenu("&File")
+	def setupMenus(self):
+		fileMenu = self.menuBar().addMenu("&File")
 
-        openAction = fileMenu.addAction("&Open...")
-        openAction.setShortcut("Ctrl+O")
+		openAction = fileMenu.addAction("&Open...")
+		openAction.setShortcut("Ctrl+O")
 
-        saveAction = fileMenu.addAction("&Save...")
-        openAction.setShortcut("Ctrl+S")
+		saveAction = fileMenu.addAction("&Save...")
+		openAction.setShortcut("Ctrl+S")
 
-        exitAction = fileMenu.addAction("E&xit")
-        exitAction.setShortcut("Ctrl+Q")
+		exitAction = fileMenu.addAction("E&xit")
+		exitAction.setShortcut("Ctrl+Q")
 
 
-        openAction.triggered.connect(self.load)
-        saveAction.triggered.connect(self.save)
-        exitAction.triggered.connect(QtGui.qApp.quit)
-        
+		openAction.triggered.connect(self.load)
+		saveAction.triggered.connect(self.save)
+		exitAction.triggered.connect(QtGui.qApp.quit)
+		
 
-    def setupWidgets(self):
-        frame = QtGui.QFrame()
-        frameLayout = QtGui.QHBoxLayout(frame)
+	def setupWidgets(self):
+		frame = QtGui.QFrame()
+		frameLayout = QtGui.QHBoxLayout(frame)
 
-        self.componentsList = componentsList()
+		self.componentsList = componentsList()
 
-        self.BlockWidget = BlockWidget()
+		self.BlockWidget = BlockWidget()
 
-        frameLayout.addWidget(self.componentsList)
-        frameLayout.addWidget(self.BlockWidget)
-        self.setCentralWidget(frame)
+		frameLayout.addWidget(self.componentsList)
+		frameLayout.addWidget(self.BlockWidget)
+		self.setCentralWidget(frame)
 
 
 if __name__ == '__main__':
 
-    import sys
+	import sys
 
-    app = QtGui.QApplication(sys.argv)
-    window = MainWindow()
-    window.loadComponents()
-    window.show()
-    sys.exit(app.exec_())
+	app = QtGui.QApplication(sys.argv)
+	window = MainWindow()
+	window.loadComponents()
+	window.show()
+	sys.exit(app.exec_())
--- blocks/draganddrop/blocks/component.py
+++ blocks/draganddrop/blocks/component.py
@@ -1,4 +1,4 @@
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8; mode: python; indent-tabs-mode: t; tab-width:4 -*-
 ############################################################################
 #
 #  Copyright (C) 2017 Georges Khaznadar <georgesk@debian.org>
@@ -21,206 +21,210 @@ from xml.dom.minidom import parseString
 import blocks_rc
 
 def acceptedFormats(event):
-    """
-    acceptable formats start with "image/x-Block-"
-    returns a list of accepted formats.
-    """
-    return [f for f in event.mimeData().formats() \
-                if f.contains("image/x-Block-")]
+	"""
+	acceptable formats start with "image/x-Block-"
+	returns a list of accepted formats.
+	"""
+	return [f for f in event.mimeData().formats() \
+			if f.contains("image/x-Block-")]
 
 class SnapPoint(QtCore.QPoint):
-    def __init__(self, x, y, text):
-        QtCore.QPoint.__init__(self, x, y)
-        self.text=text
-        return
-    def __str__(self):
-        return "snapPoint((%s,%s),%s)" %(self.x(), self.y(), self.text)
-        
+	def __init__(self, x, y, text):
+		QtCore.QPoint.__init__(self, x, y)
+		self.text=text
+		return
+
+	def __str__(self):
+		return "snapPoint((%s,%s),%s)" %(self.x(), self.y(), self.text)
+		
 class Component(object):
-    """
-    This class describes a programmation component, which can be
-    organized with other instances. It features a widget, summarized
-    by an icon, and each icon has some points which can be linked
-    to other components.
-    
-    When a collection of components are organized on top of some
-    canvas, they can be compiled into some usable program.
-    """
-    def __init__(self, pixmap, ident, mimetype, rect=None, hotspot=None,
-                    snapPoints=[]):
-        """
-        The constructor
-        @param pixmap a drawing to make an icon, and able to suggest
-        the function of the component
-        @param ident an identifier
-        @param mimetype a type which decides some behaviors in the user
-        interface
-        @param rect the surrounding rectangle; defaults to None, which
-        will define rect as surrounding the given pixmap
-        @param hotspot the position of the mouse during the drag of
-        the pixmap
-        """
-        super(Component, self).__init__()
-        if rect:
-            self.rect=rect
-        else:
-            self.rect=pixmap.rect()
-        if hotspot:
-            self.hotspot=hospot
-        else:
-            self.hotspot=QtCore.QPoint(pixmap.width()/2, pixmap.height()/2)
-        self.pixmap=pixmap
-        self.ident=ident
-        self.mimetype=mimetype
-        self.snapPoints=snapPoints
-        return
-        
-    def __str__(self):
-        return "Component(%s, %s, %s, %s, %s)" \
-                    %(self.ident, self.mimetype, self.rect, self.hotspot,
-                        self.snapPoints
-                    )        
+	"""
+	This class describes a programmation component, which can be
+	organized with other instances. It features a widget, summarized
+	by an icon, and each icon has some points which can be linked
+	to other components.
+	
+	When a collection of components are organized on top of some
+	canvas, they can be compiled into some usable program.
+	"""
+	def __init__(self, pixmap, ident, mimetype, rect=None, hotspot=None,
+					snapPoints=[]):
+		"""
+		The constructor
+		@param pixmap a drawing to make an icon, and able to suggest
+		the function of the component
+		@param ident an identifier
+		@param mimetype a type which decides some behaviors in the user
+		interface
+		@param rect the surrounding rectangle; defaults to None, which
+		will define rect as surrounding the given pixmap
+		@param hotspot the position of the mouse during the drag of
+		the pixmap
+		"""
+		super(Component, self).__init__()
+		if rect:
+			self.rect=rect
+		else:
+			self.rect=pixmap.rect()
+		if hotspot:
+			self.hotspot=hotspot
+		else:
+			self.hotspot=QtCore.QPoint(pixmap.width()/2, pixmap.height()/2)
+		self.pixmap=pixmap
+		self.ident=ident
+		self.mimetype=mimetype
+		self.snapPoints=snapPoints
+		return
+		
+	def __str__(self):
+		return "Component(\n  %s,\n  %s,\n  rect: %s,\n  hotspot: %s,\n  snaps: %s)" \
+					%(self.ident, self.mimetype, self.rect, self.hotspot,
+						self.snapPoints
+					)		
 
-    def serialize(self):
-        """
-        serializes a component into a QDataStream
-        returns data as a QByteArray instance
-        """
-        itemData = QtCore.QByteArray()
-        dataStream = QtCore.QDataStream(itemData, QtCore.QIODevice.WriteOnly)
-        dataStream << self.pixmap << self.mimetype \
-                    << self.hotspot << self.ident \
-                    << QtCore.QVariant(len(self.snapPoints))
-        for sp in self.snapPoints:
-            dataStream << QtCore.QPoint(sp) << QtCore.QString(sp.text)
-        return itemData
-        
-    @staticmethod
-    def fromListWidgetItem(lwi):
-        """
-        creates ans returns a Component instance read from
-        a QListWidgetItem instance
-        """
-        data=lwi.data(QtCore.Qt.UserRole).toByteArray()
-        return Component.unserialize(data)
-        
-    def toListWidgetItem(self, lwi):
-        """
-        records custom data into a QListWidgetItem instance
-        """
-        lwi.mimetype = self.mimetype
-        lwi.ident=self.ident
-        lwi.setIcon(QtGui.QIcon(self.pixmap))
-        byteArray=self.serialize()
-        lwi.setData(QtCore.Qt.UserRole, byteArray)
-        return
-        
-    @staticmethod
-    def unserialize(data):
-        """
-        unserialize frome a byteArray,
-        @return a new Component instance
-        """
-        dataStream = QtCore.QDataStream(data, QtCore.QIODevice.ReadOnly)
-        pixmap = QtGui.QPixmap()
-        mimetype = QtCore.QString()
-        ident = QtCore.QString()
-        hotspot = QtCore.QPoint()
-        length = QtCore.QVariant()
-        dataStream >> pixmap >> mimetype >> hotspot >> ident \
-                    >> length
-        length, report = length.toInt()
-        sp=[]
-        for i in range(length):
-            point=QtCore.QPoint()
-            text=QtCore.QString()
-            dataStream >> point >> text
-            sp.append(SnapPoint(point.x(), point.y(), text))
-        return Component(pixmap,ident,mimetype,snapPoints=sp)
-        
-    @staticmethod
-    def unserializeFromEvent(event):
-        """
-        userialize given QEvent's data into a Component instance
-        @param event a QEvent, presumably due to a drop.
-        @return an instance of Component 
-        """
-        f = acceptedFormats(event)
-        if f:
-            data = event.mimeData().data(f[0])
-            result=Component.unserialize(data)
-            result.rect = QtCore.QRect((event.pos()-result.hotspot), result.pixmap.size())
-        else:
-            result = None
-        return result
+	def serialize(self):
+		"""
+		serializes a component into a QDataStream
+		returns data as a QByteArray instance
+		"""
+		itemData = QtCore.QByteArray()
+		dataStream = QtCore.QDataStream(itemData, QtCore.QIODevice.WriteOnly)
+		dataStream << self.pixmap << self.mimetype \
+				<< self.hotspot << self.ident \
+				<< QtCore.QVariant(len(self.snapPoints))
+		for sp in self.snapPoints:
+			dataStream << QtCore.QPoint(sp) << QtCore.QString(sp.text)
+		return itemData
+		
+	@staticmethod
+	def fromListWidgetItem(lwi):
+		"""
+		creates ans returns a Component instance read from
+		a QListWidgetItem instance
+		"""
+		return lwi.component
+		
+	def toListWidgetItem(self, lwi):
+		"""
+		records custom data into a QListWidgetItem instance
+		"""
+		lwi.component=self
+		lwi.setIcon(QtGui.QIcon(self.pixmap))
+		"""
+		byteArray=self.serialize()
+		lwi.setData(QtCore.Qt.UserRole, byteArray)
+		"""
+		return
+		
+	@staticmethod
+	def unserialize(data):
+		"""
+		unserialize frome a byteArray,
+		@return a new Component instance
+		"""
+		dataStream = QtCore.QDataStream(data, QtCore.QIODevice.ReadOnly)
+		pixmap = QtGui.QPixmap()
+		mimetype = QtCore.QString()
+		ident = QtCore.QString()
+		hotspot = QtCore.QPoint()
+		length = QtCore.QVariant()
+		dataStream >> pixmap >> mimetype >> hotspot >> ident >> length
+		length, report = length.toInt()
+		sp=[]
+		for i in range(length):
+			point=QtCore.QPoint()
+			text=QtCore.QString()
+			dataStream >> point >> text
+			sp.append(SnapPoint(point.x(), point.y(), text))
+		return Component(pixmap,ident,mimetype,hotspot=hotspot,snapPoints=sp)
+		
+	@staticmethod
+	def unserializeFromEvent(event):
+		"""
+		userialize given QEvent's data into a Component instance
+		@param event a QEvent, presumably due to a drop.
+		@return an instance of Component 
+		"""
+		f = acceptedFormats(event)
+		if f:
+			data = event.mimeData().data(f[0])
+			result=Component.unserialize(data)
+			result.rect = QtCore.QRect(
+					(event.pos()-result.hotspot),
+					result.pixmap.size()
+			)
+		else:
+			result = None
+		return result
 
-    @staticmethod
-    def listFromRC():
-        """
-        gets a list of components from the application's QRC file
-        """
-        componentDirPattern = re.compile(r"components(.)")
-        result=[]
-        # browse top-level directories of the resource file
-        for rcDir in sorted(QtCore.QDir(":/").entryList()):
-            # directory's name matches r"components(.)" ???
-            m=componentDirPattern.match(rcDir)
-            if not m:
-                continue
-            else:
-                mimetype = "image/x-Block-"+m.group(1)
-            d=QtCore.QDir(":/"+rcDir)
-            # browse SVG files contained in those directories
-            for entry in sorted(d.entryList()):
-                imgPath=":/"+rcDir+"/"+entry
-                img=QtGui.QPixmap(imgPath)
-                sp=snapPoints(imgPath)
-                result.append(Component(img, entry, mimetype, snapPoints=sp))
-        return result
-        
-    def makeDrag(self, parent):
-        """
-        creates and returns a QDrag object with the given parent
-        @param parent a window, where a drag is starting
-        @return the DQrag instance
-        """
-        itemData = self.serialize()
+	@staticmethod
+	def listFromRC():
+		"""
+		gets a list of components from the application's QRC file
+		"""
+		componentDirPattern = re.compile(r"components(.)")
+		result=[]
+		# browse top-level directories of the resource file
+		for rcDir in sorted(QtCore.QDir(":/").entryList()):
+			# directory's name matches r"components(.)" ???
+			m=componentDirPattern.match(rcDir)
+			if not m:
+				continue
+			else:
+				mimetype = "image/x-Block-"+m.group(1)
+			d=QtCore.QDir(":/"+rcDir)
+			# browse SVG files contained in those directories
+			for entry in sorted(d.entryList()):
+				imgPath=":/"+rcDir+"/"+entry
+				img=QtGui.QPixmap(imgPath)
+				sp=snapPoints(imgPath)
+				result.append(Component(img, entry, mimetype, snapPoints=sp))
+		return result
+		
+	def makeDrag(self, parent):
+		"""
+		creates and returns a QDrag object with the given parent
+		@param parent a window, where a drag is starting
+		@return the DQrag instance
+		"""
+		itemData = self.serialize()
 
-        mimeData = QtCore.QMimeData()
-        mimeData.setData(self.mimetype, itemData)
+		mimeData = QtCore.QMimeData()
+		mimeData.setData(self.mimetype, itemData)
 
-        drag = QtGui.QDrag(parent)
-        drag.setMimeData(mimeData)
-        drag.setHotSpot(self.hotspot)
-        drag.setPixmap(self.pixmap)
-        return drag
-        
+		drag = QtGui.QDrag(parent)
+		drag.setMimeData(mimeData)
+		drag.setHotSpot(self.hotspot)
+		drag.setPixmap(self.pixmap)
+		return drag
+		
 def snapPoints(rcpath):
-    """
-    @result a list of snapPoints. Those are centers of circles
-    available in the SVG picture, denoted byids which begin with
-    "block-";those circles may be invisible in the pixmap.
-    """
-    f=QtCore.QFile(rcpath)
-    f.open(QtCore.QIODevice.ReadOnly | QtCore.QIODevice.Text)
-    svg=f.readAll()
-    f.close()
-    svgDoc=parseString(svg)
-    firstgroup=svgDoc.getElementsByTagName("g")[0]
-    trans=firstgroup.getAttribute("transform")
-    xt,yt=re.match(r"translate\((.*),(.*)\)",trans).groups()
-    xt=float(xt);yt=float(yt)
-    circles=svgDoc.getElementsByTagName("circle")
-    snapCircles=[c for c in circles if re.match(r"^block-",c.getAttribute("id"))]
-    result=[]
-    for c in circles:
-        xc=float(c.getAttribute("cx"))
-        yc=float(c.getAttribute("cy"))
-        id_=c.getAttribute("id")
-        result.append(SnapPoint(xc-xt, yc-yt, id_))
-    return result
-    
+	"""
+	@result a list of snapPoints. Those are centers of circles
+	available in the SVG picture, denoted byids which begin with
+	"block-";those circles may be invisible in the pixmap.
+	"""
+	f=QtCore.QFile(rcpath)
+	f.open(QtCore.QIODevice.ReadOnly | QtCore.QIODevice.Text)
+	svg=f.readAll()
+	f.close()
+	svgDoc=parseString(svg)
+	firstgroup=svgDoc.getElementsByTagName("g")[0]
+	trans=firstgroup.getAttribute("transform")
+	xt,yt=re.match(r"translate\((.*),(.*)\)",trans).groups()
+	xt=float(xt);yt=float(yt)
+	circles=svgDoc.getElementsByTagName("circle")
+	snapCircles=[c for c in circles \
+				 if re.match(r"^block-",c.getAttribute("id"))]
+	result=[]
+	for c in circles:
+		xc=float(c.getAttribute("cx"))
+		yc=float(c.getAttribute("cy"))
+		id_=c.getAttribute("id")
+		result.append(SnapPoint(xc+xt, yc+yt, id_))
+	return result
+	
 if __name__=="__main__":
-    import sys
-    app = QtGui.QApplication(sys.argv)
-    Component.listFromRC()
+	import sys
+	app = QtGui.QApplication(sys.argv)
+	Component.listFromRC()
