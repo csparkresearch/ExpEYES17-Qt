e2b617c4a5b85811560d0b7b3be22743a9ae0deb
Georges Khaznadar <georgesk@debian.org>
2017-04-17 10:16:21 +0200

    separated classes BlockWidget and ComponentsList in different files

--- blocks/draganddrop/blocks/blocks.py
+++ blocks/draganddrop/blocks/blocks.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python
 # -*- coding: utf-8; mode: python; indent-tabs-mode: t; tab-width:4 -*-
-############################################################################
-#
-#  Copyright (C) 2017 Georges Khaznadar <georgesk@debian.org>
-#
-#
-#  This file may be used under the terms of the GNU General Public
-#  License version 3.0 as published by the Free Software Foundation,
-#  or, at your preference, any later verion of the same.
-#
-#  This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-#  WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-#
-############################################################################
 
 from __future__ import print_function
+
+license="""\
+  Copyright (C) 2017 Georges Khaznadar <georgesk@debian.org>
+
+  Application Expeyes-Blocks
+
+  This application may be used under the terms of the
+  GNU General Public License version 3.0 as published by
+  the Free Software Foundation, or, at your preference,
+   any later verion of the same.
+
+  Expeyes-Blocks is built upon Qt4 GUI libraries, see "About Qt".
+
+  This application is provided AS IS with NO WARRANTY OF ANY KIND,
+  INCLUDING THE WARRANTY OF DESIGN, MERCHANTABILITY AND
+  FITNESS FOR A PARTICULAR PURPOSE.
+"""
 
 import copy
 
@@ -22,273 +26,13 @@ from PyQt4.QtCore import QPoint, QRect, Qt, QSize, QString, QTimer
 
 from PyQt4.QtGui import QWidget, QPixmap, QSizePolicy, QColor, \
 		QPainter, QListWidget, QListWidgetItem, QMainWindow, \
-		qApp, QFrame, QApplication, QHBoxLayout, QListView
+		qApp, QFrame, QApplication, QHBoxLayout, QListView, QMessageBox
 
+from templates.ui_blocks import Ui_MainWindow
 from component import Component, InputComponent
 from timecomponent import TimeComponent
 #from modifcomponent import ModifComponent
 #from channelcomponent import ChannelComponent
-
-class BlockWidget(QWidget):
-
-	def __init__(self, parent=None):
-		super(BlockWidget, self).__init__(parent)
-
-		self.comp = None #dragged component
-		self.components = []
-		self.hots = [] # list of matching snap points
-
-		self.setAcceptDrops(True)
-		self.setMinimumSize(400, 400)
-		self.hotPx={
-			"red": QPixmap(":/hot/hot-red.svg"),
-		}
-
-	def clear(self):
-		self.components = []
-		self.hots=[]
-		self.update()
-
-	def dragEnterEvent(self, event):
-		if Component.acceptedFormats(event):
-		   event.accept()
-		else:
-			event.ignore()
-
-	def dragMoveEvent(self, event):
-		f=Component.acceptedFormats(event)
-		if f:
-			data = event.mimeData().data(f[0])
-			comp, dataStream, className = Component.unserialize(data)
-			offset=comp.hotspot
-			# temporarily erase previous hot marks
-			previouslyHots=self.hots
-			self.hots=[]
-			match=False
-			for sp in comp.snapPoints:
-				hovering=event.pos()-offset+sp
-				for flavors in Component.matchingFlavors:
-					for m in self.matchingComponentSnap(hovering,sp,flavors):
-						self.hots.append(m[0].rect.topLeft()+m[1])
-						match=True
-			if match or len(previouslyHots) != len(self.hots):
-				self.update()
-				
-							
-			event.setDropAction(Qt.MoveAction)
-			event.accept()
-		else:
-			event.ignore()
-		return
-
-	def matchingComponentSnap(self, pos, snapPoint, flavors):
-		"""
-		finds components underlying a snap point, with given position
-		and a couple of flavors
-		@param pos the current position of the snap point
-		@param snapPoint the snap point
-		@param flavors a couple of texts for matching snap points
-		@return a list of matching component and its snapPoint
-		"""
-		result=[]
-		# to implement symmetry in the flavor's relation
-		for f in flavors, (flavors[1], flavors[0]):
-			if str(snapPoint.text).startswith(f[0]):
-				for c in self.components:
-					for s in c.snapPoints:
-						if str(s.text).startswith(f[1]):
-							gap=c.rect.topLeft()+s-pos
-							if gap.manhattanLength() < 60:
-								result.append((c, s))
-		return result
-
-
-	def dropEvent(self, event):
-		comp, dataStream, className = Component.unserializeFromEvent(event)
-		if comp:
-			self.components.append(comp)
-			self.update(comp.rect)
-			self.connectSnaps()
-			event.setDropAction(Qt.MoveAction)
-			event.accept()
-		else:
-			event.ignore()
-
-	def connectSnaps(self):
-		"""
-		moves components until every connected snaps overlap
-		"""
-		toMove=[] # list of components to move
-		for c in self.components: c.reset()
-		for c in self.components:
-			c.touch()
-			for s in c.snapPoints:
-				p=c.rect.topLeft()+s
-				for flavors in Component.matchingFlavors:
-					for c1, s1 in self.matchingComponentSnap(p,s,flavors):
-						# do not move already touched components
-						if c1.touched_: continue
-						toMove.append((c,s,c1,s1))
-						c1.touch()
-		for c,s,c1,s1 in toMove:
-			delta=c.rect.topLeft()+s-c1.rect.topLeft()-s1
-			c1.rect.translate(delta)
-		self.update()
-		QTimer.singleShot(1000, self.hideHots)
-		return
-
-	def hideHots(self):
-		self.hots=[]
-		self.update()
-
-	def mousePressEvent(self, event):
-		if event.buttons() == Qt.LeftButton:
-			comps=self.targetComps(event.pos())
-			if not comps:
-				return
-			comp = comps[-1]
-			index=self.components.index(comp)
-			comp=copy.copy(comp)
-
-			del self.components[index]
-
-			self.update(comp.rect)
-
-			comp.hotspot=QPoint(event.pos() - comp.rect.topLeft())
-			drag=comp.makeDrag(self)
-
-			if drag.exec_(Qt.MoveAction) != Qt.MoveAction:
-				self.components.insert(index, self.comp)
-				self.update()
-		elif event.buttons() == Qt.RightButton:
-			b=self.blockAt(event.pos())
-			if b:
-				i = self.components.index(b)
-				if isinstance(b, InputComponent):
-					self.components[i]=TimeComponent.fromOther(b)
-				self.update()
-
-	def blockAt(self, pos):
-		"""
-		@param pos a QPoint instance
-		@return the component at the given position if any, else None
-		"""
-		result=None
-		for c in self.components:
-			if c.rect.contains(pos):
-				mask=c.pixmap.mask()
-				color=QColor(mask.toImage().pixel(
-						pos-c.rect.topLeft())
-				).getRgb()
-				if color[0]==0:
-					# not masked pixel
-					return c	
-		return None
-
-	def paintEvent(self, event):
-		painter = QPainter()
-		painter.begin(self)
-		painter.fillRect(event.rect(), Qt.white)
-		
-		for c in self.components:
-			c.draw(painter)
-
-		# hot indicators
-		if self.hots:
-			px=self.hotPx["red"]
-			middle = QPoint(px.size().width()/2, px.size().height()/2)
-		for hot in self.hots:
-			# paint a circle around
-			painter.drawPixmap(QRect(hot-middle,px.size()),px)
-			
-		painter.end()
-
-	def targetComps(self, position):
-		"""
-		returns the list of components
-		under a mouse click; the topmost component comes last.
-		"""
-		comps = [c for c in self.components if c.rect.contains(position)]
-		return comps
-
-class componentsList(QListWidget):
-	def __init__(self, parent=None):
-		super(componentsList, self).__init__(parent)
-
-		self.setDragEnabled(True)
-		self.setViewMode(QListView.IconMode)
-		self.setIconSize(QSize(60, 60))
-		self.setSpacing(10)
-		self.setAcceptDrops(True)
-		self.setDropIndicatorShown(True)
-		self.setSizePolicy(QSizePolicy(QSizePolicy.Fixed,
-				QSizePolicy.Expanding))
-		self.setMinimumSize(200, 400)
-		self.setMaximumSize(200, 4000)
-
-	def dragEnterEvent(self, event):
-		if Component.acceptedFormats(event):
-			event.accept()
-		else:
-			event.ignore()
-
-	def dragMoveEvent(self, event):
-		if Component.acceptedFormats(event):
-			event.setDropAction(Qt.MoveAction)
-			event.accept()
-		else:
-			event.ignore()
-
-	def dropEvent(self, event):
-		comp, dataStream, className = Component.unserializeFromEvent(event)
-		if comp:
-			# components of type 1 can be duplicated
-			# so they should not be appended to the list
-			if comp.mimetype.contains("image/x-Block-1"):
-				pass
-			elif comp.mimetype.contains("image/x-Block-2"):
-				self.addPiece(comp)
-
-			event.setDropAction(Qt.MoveAction)
-			event.accept()
-		else:
-			event.ignore()
-
-	def newComponent(self, comp):
-		item=self.addPiece(comp)
-		self.insertItem(0, item)
-		return
-
-	def addPiece(self, comp):
-		"""
-		adds a Component instance,
-		and returns the QListWidgetItem created
-		"""
-		ident=QString(comp.ident)
-		for i in range(self.count()):
-			if self.item(i).component.ident == ident:
-				self.item(i).setHidden(False)
-				return
-		blockItem = QListWidgetItem(self)
-		comp.toListWidgetItem(blockItem)
-		return blockItem
-
-	def currentComponent(self):
-		item = self.currentItem()
-		return Component.fromListWidgetItem(item)
-		
-		
-	def startDrag(self, supportedActions):
-		component=self.currentComponent()
-		drag=component.makeDrag(self)
-
-		if drag.exec_(Qt.MoveAction) == Qt.MoveAction:
-			# components of type 1 can be duplicated
-			# so they should not be hidden from the list
-			if component.mimetype.contains("image/x-Block-1"):
-				pass
-			elif component.mimetype.contains("image/x-Block-2"):
-				self.currentItem().setHidden(True)
 
 
 class MainWindow(QMainWindow):
@@ -301,25 +45,6 @@ class MainWindow(QMainWindow):
 		self.setupWidgets()
 
 		self.setWindowTitle("Block")
-		
-	def load(self):
-		"""
-		Loads a component composition
-		"""
-		return
-		
-	def save(self):
-		"""
-		Saves the current component composition
-		"""
-		return
-		
-	def saveAs(self, filename=None):
-		"""
-		Saves the current component composition in a new file
-		@param filename name of the file, defaults to None
-		"""
-		return
 		
 	def loadComponents(self, path=None):
 		self.componentsList.clear()
@@ -359,12 +84,64 @@ class MainWindow(QMainWindow):
 		frameLayout.addWidget(self.BlockWidget)
 		self.setCentralWidget(frame)
 
+class BlockMainWindow(QMainWindow, Ui_MainWindow):
+	def __init__(self, parent=None):
+		QMainWindow.__init__(self, parent)
+		Ui_MainWindow.__init__(self)
+		self.setupUi(self)
+		self.loadComponents()
+		self.connectSignals()
+		return
+
+	def loadComponents(self, path=None):
+		self.widget.clear()
+		cList=Component.listFromRC()
+		for c in cList:
+			self.componentsList.newComponent(c)
+		return
+
+	def connectSignals(self):
+		"""
+		connecting signals to methods
+		"""
+		self.action_Quit.triggered.connect(self.close)
+		self.actionSave.triggered.connect(self.save)
+		self.actionSave_as.triggered.connect(self.saveAs)
+		self.action_Open.triggered.connect(self.load)
+		self.action_About.triggered.connect(self.about)
+		
+	def about(self):
+		"""
+		brings up the About dialog
+		"""
+		QMessageBox.about(self,"About", license)
+		return
+		
+	def load(self):
+		"""
+		Loads a component composition
+		"""
+		return
+		
+	def save(self):
+		"""
+		Saves the current component composition
+		"""
+		return
+		
+	def saveAs(self, filename=None):
+		"""
+		Saves the current component composition in a new file
+		@param filename name of the file, defaults to None
+		"""
+		return
+		
 if __name__ == '__main__':
 
 	import sys
 
 	app = QApplication(sys.argv)
-	window = MainWindow()
-	window.loadComponents()
+	window = BlockMainWindow()
+	#window.loadComponents()
 	window.show()
 	sys.exit(app.exec_())
